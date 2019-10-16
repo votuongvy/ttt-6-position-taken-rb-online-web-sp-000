47edae7a854732e3aeac3f2a1cd04abd9e79db4d
vy vo <tuonggvy.vo@gmail.com>
2019-10-16 14:33:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index] == " "
-    return false
-  elsif board[index] == ""
-    return false
-  elsif board[index] == nil
+  if board[index] == " " || board[index] == "" || board[index == nil
     return false
   end
 end
