4228e85f03e4068f6279623a17148b96258ab49d
vy vo <tuonggvy.vo@gmail.com>
2019-10-16 14:35:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@ def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index == nil
     return false
   elsif board[index] == "X" || board[index] == "O"
-    return true  
+    return true 
   end
 end
